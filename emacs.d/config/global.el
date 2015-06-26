b14555f4656104ba55709e31566021b1f4b72130
Daniel Patterson <dbp@dbpmail.net>
2015-06-25 21:31:38 -0700

    Remove some dead code.

--- emacs.d/config/global.el
+++ emacs.d/config/global.el
@@ -152,9 +152,6 @@
 
 (global-set-key (kbd "C-c g") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
-;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 (setq helm-quick-update                     t ; do not display invisible candidates
       helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
